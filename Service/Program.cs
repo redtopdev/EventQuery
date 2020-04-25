14648a3896ee36ad97957e12bd6633849db24c77
Mani0811 <mani.ciem@gmail.com>
2020-04-25 19:21:40 +0530

    modified

--- Service/Program.cs
+++ Service/Program.cs
@@ -4,6 +4,7 @@
 
 namespace EventQuery.Service
 {
+    using Engaze.Core.Web;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
@@ -15,35 +16,7 @@ namespace EventQuery.Service
     {
         public static void Main(string[] args)
         {
-            var configuration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json")
-            .Build();
-
-            Log.Logger = new LoggerConfiguration()
-            .ReadFrom.Configuration(configuration)
-            .CreateLogger();
-
-            try
-            {
-                Log.Information("Application starting up. ");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Application start-up failed");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)    
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            EngazeWebHost.Run<Startup>(args);
+        }       
     }
 }
--- Service/Startup.cs
+++ Service/Startup.cs
@@ -5,58 +5,27 @@ namespace EventQuery.Service
 {
     using Engaze.Core.Persistance.Cassandra;
     using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
     using EventQuery.DataPersistance;
     using Serilog;
+    using Engaze.Core.Web;
 
-    public class Startup
+    public class Startup : EngazeStartup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) : base(configuration) { }   
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public override void ConfigureComponentServices(IServiceCollection services)
         {
-            services.AddControllers();
-            services.ConfigureCassandraServices(Configuration);                     
-            services.AddTransient<IEventManager,EventManager>();
+            services.ConfigureCassandraServices(Configuration);
+            services.AddTransient<IEventManager, EventManager>();
             services.AddTransient<IEventRepository, EventRepository>();
-            
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public override void ConfigureComponent(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            //app.UseHttpsRedirection();
-
-            app.UseSerilogRequestLogging();
-
-            app.UseRouting();
-
             app.UseAuthorization();
-
-            app.UseAppException();
-
-            app.UseAppStatus();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-
-            //app.Use)
+            app.UseSerilogRequestLogging();
         }
     }
 }
